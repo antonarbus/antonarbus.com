34c537f280bb04dabc57792a1380280c538b058a
Anton Shcherbinin <anton.shcherbinin@heeros.com>
2022-10-09 20:41:31 +0300

    css prop + has selector

---
+++ posts/emotion_css_prop.js
@@ -0,0 +1,157 @@
+import { Code, H, Hs, LazyImg, Lnk, React, useEffect, useState, useRef, useCallback, useMemo, jsxToStr } from '/components/post/reExport'
+import { css } from '@emotion/react'
+
+function Component() {
+  return (
+    <div
+      css={css`
+        * {
+          border: 2px solid grey;
+          margin: 20px;
+          padding: 20px;
+          background-color: white;
+        }
+      `}
+    >
+      <div>div1</div>
+      <div css={{ background: 'red' }}>div2</div>
+    </div>
+  )
+}
+
+const postObj = {
+  title: 'emotion css prop',
+  date: '2022.10.10',
+  tags: ['css', 'React'],
+  imgUrl: 'https://antonarbus.com/imgs/emotion.png',
+  desc: 'emotion css prop',
+  body: (
+    <>
+      <H>CSS prop in Emotion library</H>
+
+      <p><Lnk url='https://emotion.sh/docs/css-prop'>The css Prop</Lnk> can take styles as template literals or object.</p>
+
+      <Code block jsx>{`
+      import { css } from '@emotion/react'
+
+      function Component() {
+        return (
+          <div
+            css={css\`
+              * {
+                border: 2px solid grey;
+                margin: 20px;
+                padding: 20px;
+                background-color: white;
+              }
+            \`}
+          >
+            <div>div1</div>
+            <div css={{ background: 'red' }}>div2</div>
+          </div>
+        )
+      }
+      `}</Code>
+
+      <Component />
+
+      <H>Configuration in Next.js</H>
+
+      <Hs>package.json</Hs>
+
+      <Code block json>{`
+      {
+        "dependencies": {
+          "@emotion/react": "^11.10.4",
+          "@emotion/styled": "^11.10.4",
+        },
+        "devDependencies": {
+          "@emotion/babel-plugin": "^11.10.2",
+          "@emotion/eslint-plugin": "^11.10.0",
+        }
+      }
+      `}</Code>
+
+      <Hs>next.config.js</Hs>
+
+      <Code block>{`
+      const nextConfig = {
+        compiler: {
+          emotion: true
+        }
+      }
+      module.exports = nextConfig
+      `}</Code>
+
+      <Hs>.babelrc</Hs>
+
+      <Code block jsx>{`
+      {
+        "presets": [
+          [
+            "next/babel",
+            {
+              "preset-react": {
+                "runtime": "automatic",
+                "importSource": "@emotion/react"
+              }
+            }
+          ]
+        ],
+        "plugins": ["@emotion/babel-plugin"]
+      }
+      `}</Code>
+
+      <Hs>.eslintrc.js</Hs>
+
+      <Code block jsx>{`
+      // .eslintrc.js
+      module.exports = {
+        env: {
+          browser: true,
+          es2021: true,
+          jest: true
+        },
+        extends: [
+          'plugin:react/recommended',
+          'standard'
+        ],
+        parser: '@typescript-eslint/parser',
+        parserOptions: {
+          ecmaFeatures: {
+            jsx: true
+          },
+          ecmaVersion: 'latest',
+          sourceType: 'module'
+        },
+        plugins: [
+          'react',
+          '@typescript-eslint',
+          '@emotion'
+        ],
+        rules: {
+          'react/react-in-jsx-scope': 'off',
+          'space-before-function-paren': 'off',
+          'react/prop-types': 'off',
+          'import/no-absolute-path': 'off',
+          'react/no-unescaped-entities': 'off',
+          'react/no-unknown-property': ['error', { ignore: ['css', 'jsx'] }]
+        }
+      }
+
+      `}</Code>
+
+    </>
+  )
+}
+
+export default postObj
+
+export const post = {
+  title: postObj.title,
+  date: postObj.date,
+  tags: postObj.tags,
+  desc: postObj.desc,
+  imgUrl: postObj.imgUrl || null,
+  bodyStr: jsxToStr(postObj.body)
+}
--- posts/has_selector.js
+++ posts/has_selector.js
@@ -1,9 +1,28 @@
 import { Code, H, Hs, LazyImg, Lnk, React, useEffect, useState, useRef, useCallback, useMemo, jsxToStr } from '/components/post/reExport'
+import { css } from '@emotion/react'
 
 function Component() {
   return (
-    <div css={{ background: 'red' }}>
-      hi
+    <div
+      css={css`
+        * {
+          border: 2px solid grey;
+          padding: 20px;
+        }
+        .inner {
+          background-color: white;
+        }
+        .outer:has(.inner) {
+          background-color: red;
+        }
+      `}
+    >
+      <div className='outer'>
+        Outer element
+        <div className='inner'>
+          Inner element
+        </div>
+      </div>
     </div>
   )
 }
@@ -16,7 +35,75 @@ const postObj = {
   desc: 'css :has parent selector in css',
   body: (
     <>
+      <H>:has selector</H>
+
+      <p>Selector selects all elements with class <code>.outer</code> which contains an element with the class <code>.inner</code> and applies red background.</p>
+
+      <Code block jsx>{`
+      function Component() {
+        return (
+          <div
+            css={css\`
+              * {
+                border: 2px solid grey;
+                padding: 20px;
+              }
+              .inner {
+                background-color: white;
+              }
+              .outer:has(.inner) {
+                background-color: red;
+              }
+            \`}
+          >
+            <div
+              className='outer'
+              // css={{ border: '1px solid grey', padding: '20px' }}
+            >
+              Outer element
+              <div
+                className='inner'
+                // css={{ border: '1px solid grey', padding: '20px' }}
+              >
+                Inner element
+              </div>
+            </div>
+          </div>
+        )
+      }
+      `}</Code>
+
       <Component />
+
+      <H>Pass multiple selectors</H>
+
+      <Code block css>{`
+      .heading:has(> .subtitle strong#accent) { }
+      `}</Code>
+
+      <p>Selects any <code>.heading</code> element that has a direct child with the class <code>.subtitle</code> which contains a <code>strong</code> element in it with the id <code>accent</code>.</p>
+
+      <H>Another example</H>
+
+      <Code block css>{`
+      .heading:has(.subtitle) p { }
+      `}</Code>
+
+      <p>Selects any <code>p</code> tag that is inside a <code>.heading</code> element as long as that <code>.heading</code> element contains a <code>.subtitle</code> element</p>
+
+      <H>Multiple has selectors</H>
+
+      <Code block css>{`
+      .heading:has(.subtitle, p) { }
+      `}</Code>
+
+      <p>Select a <code>.heading</code> that has either an element with the class of <code>.subtitle</code> inside it or a <code>p</code> element inside it </p>
+
+      <Code block css>{`
+      .heading:has(.subtitle):has(p) { }
+      `}</Code>
+
+      <p>Only select a <code>.heading</code> that has both a <code>.subtitle</code> and <code>p</code> element inside it</p>
     </>
   )
 }

c23c360c62c7b3665184645472d3c74f683418a5
sherb <sherb@Anton-MacBook-Pro.local>
2022-10-18 00:54:10 +0300

    deploy script update

--- deploy_script.js
+++ deploy_script.js
@@ -1,40 +1,64 @@
 const { exec, execSync } = require('child_process') // creates a new shell and executes a given command
+const fs = require('fs')
+
 const encoding = 'utf-8'
 const options = { encoding }
 let output
 
-console.log('01/10 start...')
+let i = 1
+console.log(i + ': starting...')
 
-output = execSync('rm -r .next archive.tar.gz', options)
-console.log(output)
-console.log('02/10 removed .next & archive folders')
+let path
+path = '.next'
+if (fs.existsSync(path)) {
+  output = execSync('rm -r ' + path, options)
+  console.log(output)
+  i++
+  console.log(i + ' removed .next')
+}
 
+path = 'archive.tar.gz'
+if (fs.existsSync(path)) {
+  i++
+  console.log(i + ': removing archive.tar.gz')
+  output = execSync('rm -r ' + path, options)
+  console.log(output)
+}
+
+i++
+console.log(i + ': building project locally')
 output = execSync('npm run build', options)
 console.log(output)
-console.log('03/10 built project locally')
 
+i++
+console.log(i + ': archiving files')
 output = execSync('tar czvf archive.tar.gz .next package.json next.config.js public', options)
 console.log(output)
-console.log('04/10 archived files')
 
+i++
+console.log(i + ': removing folders on server')
 output = execSync('ssh sherb@35.209.92.93 "cd /var/www/html/antonarbus.com/ && rm -r archive.tar.gz .next package.json next.config.js public"', options)
 console.log(output)
-console.log('05/10 removed folders on server')
 
+i++
+console.log(i + ': sending files to server')
 output = execSync('scp -r ~/Git/antonarbus.com/archive.tar.gz sherb@35.209.92.93:/var/www/html/antonarbus.com/', options)
 console.log(output)
-console.log('06/10 sent files to server')
 
+i++
+console.log(i + ': extracting files from the archive')
 output = execSync('ssh sherb@35.209.92.93 "cd /var/www/html/antonarbus.com/ && tar -xf archive.tar.gz"', options)
 console.log(output)
-console.log('07/10 extracted files from the archive')
 
+i++
+console.log(i + ': installing packages')
 output = execSync('ssh sherb@35.209.92.93 "source ~/.nvm/nvm.sh && cd /var/www/html/antonarbus.com/ && npm i -f"', options)
 console.log(output)
-console.log('08/10 installed packages')
 
+i++
+console.log(i + ': restarting the app')
 output = execSync('ssh sherb@35.209.92.93 "source ~/.nvm/nvm.sh && pm2 restart app"', options)
 console.log(output)
-console.log('09/10 restarted the app')
 
-console.log('10/10 done!')
+i++
+console.log(i + ': done!')

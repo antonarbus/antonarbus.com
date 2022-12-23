581e93ff5cd6d6d53c50ddab0ee55a6fc9edd13e
Anton Arbus <anton.arbus@macbookpro>
2022-12-24 00:33:58 +0200

    uuid

--- posts/uuid.js
+++ posts/uuid.js
@@ -1,16 +1,16 @@
 import { Code, H, Lnk, jsxToStr } from '/components/post/reExport'
 import { v1 as uuidv1, v4 as uuidv4 } from 'uuid'
-import shortid from 'shortid'
+import { nanoid } from 'nanoid'
 
-function UuidExample() {
-  return (
-    <>
-      <div>uuidv1(): <strong>{uuidv1()}</strong></div>
-      <div>uuidv4(): <strong>{uuidv4()}</strong></div>
-      <div>shortid(): <strong>{shortid()}</strong></div>
-    </>
-  )
-}
+const UuidExample = () => (
+  <>
+    <div>uuidv1(): <strong>{uuidv1()}</strong></div>
+    <div>uuidv4(): <strong>{uuidv4()}</strong></div>
+    <div>nanoid(): <strong>{nanoid()}</strong></div>
+    <div>nanoid(5): <strong>{nanoid(5)}</strong></div>
+    <div>crypto.randomUUID(): <strong>{crypto.randomUUID()}</strong></div>
+  </>
+)
 
 const postObj = {
   title: 'uuid',
@@ -19,6 +19,27 @@ const postObj = {
   desc: 'Several packages for universally unique identifier (UUID)',
   body: (
     <>
+      <H>Example</H>
+
+      <UuidExample />
+
+      <H>Example</H>
+
+      <Code block js>{`
+      import { v1 as uuidv1, v4 as uuidv4 } from 'uuid'
+      import { nanoid } from 'nanoid'
+
+      const UuidExample = () => (
+        <>
+          <div>uuidv1(): <strong>{uuidv1()}</strong></div>
+          <div>uuidv4(): <strong>{uuidv4()}</strong></div>
+          <div>nanoid(): <strong>{nanoid()}</strong></div>
+          <div>nanoid(5): <strong>{nanoid(5)}</strong></div>
+          <div>crypto.randomUUID(): <strong>{crypto.randomUUID()}</strong></div>
+        </>
+      )
+      `}</Code>
+
       <H>Math.random</H>
 
       <ul>
@@ -28,11 +49,11 @@ const postObj = {
         <li>But a better way to use existing libraries</li>
       </ul>
 
-      <H>shortid</H>
+      <H>nanoid</H>
 
       <ul>
-        <li><Lnk path={'https://www.npmjs.com/package/shortid'}>shortid</Lnk> package</li>
-        <li>Install a package with <Code bash>npm i shortid</Code></li>
+        <li><Lnk path={'https://www.npmjs.com/package/nanoid'}>nanoid</Lnk> package</li>
+        <li>Install a package with <Code bash>npm i nanoid</Code></li>
       </ul>
 
       <H>uuid</H>
@@ -42,25 +63,12 @@ const postObj = {
         <li>Install a package with <Code bash>npm i uuid</Code></li>
       </ul>
 
-      <H>Example</H>
+      <H>crypto.randomUUID()</H>
 
-      <Code block js>{`
-        import { v1 as uuidv1 } from 'uuid';
-        import { v4 as uuidv4 } from 'uuid';
-        import shortid from 'shortid';
-        
-        function UuidExample() {
-          return (
-            <>
-              <div>uuidv1(): <strong>{uuidv1()}</strong></div>
-              <div>uuidv4(): <strong>{uuidv4()}</strong></div>
-              <div>shortid(): <strong>{shortid()}</strong></div>
-            </>
-          )
-        }
-      `}</Code>
-
-      <UuidExample />
+      <ul>
+        <li>Browsers and node supports a uuid generator now - <Lnk path='https://developer.mozilla.org/en-US/docs/Web/API/Crypto/randomUUID'>crypto.randomUUID()</Lnk></li>
+        <li><Code>crypto.randomUUID()</Code> and generates similar string <i>"36b8f84d-df4e-4d49-b662-bcde71a8764f"</i></li>
+      </ul>
     </>
   )
 }

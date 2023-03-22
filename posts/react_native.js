d6b5e99e13277c8a08cec9bd1ca20c6d981cfb3a
Anton Arbus <anton.arbus@macbookpro>
2023-03-22 19:36:50 +0200

    react-native

--- posts/react_native.js
+++ posts/react_native.js
@@ -1709,6 +1709,103 @@ const postObj = {
       />
       `}</Code>
 
+      <H>Combine different navigators</H>
+
+      <ul>
+        <li>Stack navigation is the best, but in some apps we may need also a drawer and bottom tabs to navigate between screens</li>
+        <li>We can combine different navigators</li>
+      </ul>
+
+      <Code block jsx>{`
+      import { StatusBar } from 'expo-status-bar';
+      import { StyleSheet, Button } from 'react-native';
+      import { NavigationContainer } from '@react-navigation/native';
+      import { createNativeStackNavigator } from '@react-navigation/native-stack';
+      import { createDrawerNavigator } from '@react-navigation/drawer';
+      import { Ionicons } from '@expo/vector-icons';
+
+      import CategoriesScreen from './screens/CategoriesScreen';
+      import MealsOverviewScreen from './screens/MealsOverviewScreen';
+      import MealDetailScreen from './screens/MealDetailScreen';
+      import FavoritesScreen from './screens/FavoritesScreen';
+
+      const Stack = createNativeStackNavigator();
+      const Drawer = createDrawerNavigator();
+
+      function DrawerNavigator() {
+        return (
+          <Drawer.Navigator
+            screenOptions={{
+              headerStyle: { backgroundColor: '#351401' },
+              headerTintColor: 'white',
+              sceneContainerStyle: { backgroundColor: '#3f2f25' },
+              drawerContentStyle: { backgroundColor: '#351401' },
+              drawerInactiveTintColor: 'white',
+              drawerActiveTintColor: '#351401',
+              drawerActiveBackgroundColor: '#e4baa1',
+            }}
+          >
+            <Drawer.Screen
+              name="Categories"
+              component={CategoriesScreen}
+              options={{
+                title: 'All Categories',
+                drawerIcon: ({ color, size }) => (
+                  <Ionicons name="list" color={color} size={size} />
+                ),
+              }}
+            />
+            <Drawer.Screen
+              name="Favorites"
+              component={FavoritesScreen}
+              options={{
+                drawerIcon: ({ color, size }) => (
+                  <Ionicons name="star" color={color} size={size} />
+                ),
+              }}
+            />
+          </Drawer.Navigator>
+        );
+      }
+
+      export default function App() {
+        return (
+          <>
+            <StatusBar style="light" />
+            <NavigationContainer>
+              <Stack.Navigator
+                screenOptions={{
+                  headerStyle: { backgroundColor: '#351401' },
+                  headerTintColor: 'white',
+                  contentStyle: { backgroundColor: '#3f2f25' },
+                }}
+              >
+                <Stack.Screen
+                  name="Drawer"
+                  component={DrawerNavigator}
+                  options={{
+                    headerShown: false,
+                  }}
+                />
+                <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+                <Stack.Screen
+                  name="MealDetail"
+                  component={MealDetailScreen}
+                  options={{
+                    title: 'About the Meal',
+                  }}
+                />
+              </Stack.Navigator>
+            </NavigationContainer>
+          </>
+        );
+      }
+
+      const styles = StyleSheet.create({
+        container: {},
+      });
+      `}</Code>
+
   
     </>
   )

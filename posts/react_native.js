e4f6d47891bc2640caf9695a765047aeaa4a0002
Anton Arbus <anton.arbus@macbookpro>
2023-03-26 21:12:13 +0300

    react-native

--- posts/react_native.js
+++ posts/react_native.js
@@ -2965,7 +2965,128 @@ const postObj = {
         <li>Can be utilized for ex. in alarm, reminder or to-do apps, etc...</li>
         <li><Lnk path='https://docs.expo.dev/versions/latest/sdk/notifications/'>https://docs.expo.dev/versions/latest/sdk/notifications/</Lnk></li>
         <li><Code>npx expo install expo-notifications</Code></li>
+        <li>Add some configuration from the link into the <code>app.json</code></li>
+        <li>Explanation of <code>content</code> prop <Lnk path='https://docs.expo.dev/versions/latest/sdk/notifications/#notificationcontentinput'>https://docs.expo.dev/versions/latest/sdk/notifications/#notificationcontentinput</Lnk></li>
+        <li>Explanation of <code>trigger</code> prop <Lnk path='https://docs.expo.dev/versions/latest/sdk/notifications/#notificationtriggerinput'>https://docs.expo.dev/versions/latest/sdk/notifications/#notificationtriggerinput</Lnk></li>
       </ul>
+
+      <Code block jsx>{`
+      // app.json
+      {
+        "expo": {
+          "name": "RNCourse",
+          "slug": "RNCourse",
+          "version": "1.0.0",
+          "orientation": "portrait",
+          "icon": "./assets/icon.png",
+          "splash": {
+            "image": "./assets/splash.png",
+            "resizeMode": "contain",
+            "backgroundColor": "#ffffff"
+          },
+          "updates": {
+            "fallbackToCacheTimeout": 0
+          },
+          "assetBundlePatterns": [
+            "**/*"
+          ],
+          "ios": {
+            "supportsTablet": true
+          },
+          "android": {
+            "adaptiveIcon": {
+              "foregroundImage": "./assets/adaptive-icon.png",
+              "backgroundColor": "#FFFFFF"
+            }
+          },
+          "web": {
+            "favicon": "./assets/favicon.png"
+          },
+          "plugins": [
+            [
+              "expo-notifications",
+              {
+                "icon": "./local/assets/icon.png",
+                "color": "#ffffff"
+              }
+            ]
+          ]
+        }
+      }
+      `}</Code>
+
+      <Code block jsx>{`
+        // App.js
+        import { useEffect } from 'react';
+        import { StatusBar } from 'expo-status-bar';
+        import { StyleSheet, Button, View } from 'react-native';
+        import * as Notifications from 'expo-notifications';
+
+        Notifications.setNotificationHandler({
+          handleNotification: async () => {
+            return {
+              shouldPlaySound: false,
+              shouldSetBadge: false,
+              shouldShowAlert: true
+            };
+          }
+        });
+
+        export default function App() {
+          useEffect(() => {
+            const subscription1 = Notifications.addNotificationReceivedListener((notification) => {
+              console.log('NOTIFICATION RECEIVED');
+              console.log(notification);
+              const userName = notification.request.content.data.userName;
+              console.log(userName);
+            });
+
+            const subscription2 = Notifications.addNotificationResponseReceivedListener((response) => {
+              console.log('NOTIFICATION RESPONSE RECEIVED');
+              console.log(response);
+              const userName = response.notification.request.content.data.userName;
+              console.log(userName);
+            });
+
+            return () => {
+              subscription1.remove();
+              subscription2.remove();
+            };
+          }, []);
+
+          function scheduleNotificationHandler() {
+            Notifications.scheduleNotificationAsync({
+              content: {
+                title: 'My first local notification',
+                body: 'This is the body of the notification.',
+                data: { userName: 'Max' }
+              },
+              trigger: {
+                seconds: 5
+              }
+            });
+          }
+
+          return (
+            <View style={styles.container}>
+              <Button
+                title="Schedule Notification"
+                onPress={scheduleNotificationHandler}
+              />
+              <StatusBar style="auto" />
+            </View>
+          );
+        }
+
+        const styles = StyleSheet.create({
+          container: {
+            flex: 1,
+            backgroundColor: '#fff',
+            alignItems: 'center',
+            justifyContent: 'center',
+          },
+        });
+      `}</Code>
 
       <H>Push notifications</H>
 

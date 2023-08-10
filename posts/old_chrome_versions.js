d7fc9f178fc3250a354eadde0041b5a3d37b8abb
Anton Arbus <anton.arbus@macbookpro>
2023-08-10 12:34:36 +0300

    old chrome vers

---
+++ posts/old_chrome_versions.js
@@ -0,0 +1,27 @@
+import { Code, H, Hs, LazyImg, Lnk, React, useEffect, useState, useRef, useCallback, useMemo, jsxToStr } from '/components/post/reExport'
+
+const postObj = {
+  title: 'old chrome version',
+  date: '2023.08.10',
+  tags: ['tools'],
+  imgUrl: 'https://antonarbus.com/imgs/chrome.png',
+  desc: 'old chrome version',
+  body: (
+    <>
+      <H>Old chrome versions</H>
+
+      <Lnk path='https://chromium.cypress.io/'>https://chromium.cypress.io/</Lnk>
+    </>
+  )
+}
+
+export default postObj
+
+export const post = {
+  title: postObj.title,
+  date: postObj.date,
+  tags: postObj.tags,
+  desc: postObj.desc,
+  imgUrl: postObj.imgUrl || null,
+  bodyStr: jsxToStr(postObj.body)
+}

12fe8adf82c0677a21a40aed4aa1e05191d89259
Anton Arbus <anton.arbus@macbookpro>
2024-03-02 01:23:09 +0200

    file to hash

---
+++ posts/file_to_hash.js
@@ -0,0 +1,29 @@
+import { Code, H, Hs, LazyImg, Lnk, React, useEffect, useState, useRef, useCallback, useMemo, jsxToStr } from '/components/post/reExport'
+
+const postObj = {
+  title: 'file to hash',
+  date: '2024.03.02',
+  tags: ['node'],
+  imgUrl: 'https://antonarbus.com/imgs/xxx.png',
+  desc: 'file to hash',
+  body: (
+    <>
+      <H>file to hash</H>
+
+      <Code block jsx>{`
+        fileHash = crypto.createHash('sha256').update(file.buffer).digest('hex')
+      `}</Code>
+    </>
+  )
+}
+
+export default postObj
+
+export const post = {
+  title: postObj.title,
+  date: postObj.date,
+  tags: postObj.tags,
+  desc: postObj.desc,
+  imgUrl: postObj.imgUrl || null,
+  bodyStr: jsxToStr(postObj.body)
+}

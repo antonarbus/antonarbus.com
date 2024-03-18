b82cb53ee2056a32af3d824dd36bedb5f413208c
Anton Arbus <anton.arbus@macbookpro>
2024-03-18 15:06:51 +0200

    xxx

--- pages/api/allPosts.js
+++ pages/api/allPosts.js
@@ -3,6 +3,7 @@ import * as module from '/exportAllPosts.js'
 export default function handler(req, res) {
   const fileNames = Object.keys(module)
   const posts = Object.values(module)
+
   posts.forEach((post, index) => {
     post.fileName = fileNames[index]
     post.url = `/posts/${fileNames[index]}`

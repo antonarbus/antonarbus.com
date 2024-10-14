2c442038914b6b37b544bdddc5d61d3c288d8969
Anton Arbus <anton.arbus@macbookpro>
2024-10-14 08:30:59 +0300

    playwright

--- posts/playwright.js
+++ posts/playwright.js
@@ -41,66 +41,66 @@ const postObj = {
 
       <Code block jsx>{`
         // Auto-retrying assertions
-        await expect(locator).toBeAttached() - element is attached
-        await expect(locator).toBeChecked() - checkbox is checked
-        await expect(locator).toBeDisabled() - element is disabled
-        await expect(locator).toBeEditable() - element is editable
-        await expect(locator).toBeEmpty() - container is empty
-        await expect(locator).toBeEnabled() - element is enabled
-        await expect(locator).toBeFocused() - element is focused
-        await expect(locator).toBeHidden() - element is not visible
-        await expect(locator).toBeInViewport() - element intersects viewport
-        await expect(locator).toBeVisible() - element is visible
-        await expect(locator).toContainText() - element contains text
-        await expect(locator).toHaveAccessibleDescription() - element has a matching accessible description
-        await expect(locator).toHaveAccessibleName() - element has a matching accessible name
-        await expect(locator).toHaveAttribute() - element has a DOM attribute
-        await expect(locator).toHaveClass() - element has a class property
-        await expect(locator).toHaveCount() - list has exact number of children
-        await expect(locator).toHaveCSS() - element has CSS property
-        await expect(locator).toHaveId() - element has an ID
-        await expect(locator).toHaveJSProperty() - element has a JavaScript property
-        await expect(locator).toHaveRole() - element has a specific ARIA role
-        await expect(locator).toHaveScreenshot() - element has a screenshot
-        await expect(locator).toHaveText() - element matches text
-        await expect(locator).toHaveValue() - input has a value
-        await expect(locator).toHaveValues() - select has options selected
-        await expect(page).toHaveScreenshot() - page has a screenshot
-        await expect(page).toHaveTitle() - page has a title
-        await expect(page).toHaveURL() - page has a URL
-        await expect(response).toBeOK() - response has an OK status
+        await expect(locator).toBeAttached() // element is attached
+        await expect(locator).toBeChecked() // checkbox is checked
+        await expect(locator).toBeDisabled() // element is disabled
+        await expect(locator).toBeEditable() // element is editable
+        await expect(locator).toBeEmpty() // container is empty
+        await expect(locator).toBeEnabled() // element is enabled
+        await expect(locator).toBeFocused() // element is focused
+        await expect(locator).toBeHidden() // element is not visible
+        await expect(locator).toBeInViewport() // element intersects viewport
+        await expect(locator).toBeVisible() // element is visible
+        await expect(locator).toContainText() // element contains text
+        await expect(locator).toHaveAccessibleDescription() // element has a matching accessible description
+        await expect(locator).toHaveAccessibleName() // element has a matching accessible name
+        await expect(locator).toHaveAttribute() // element has a DOM attribute
+        await expect(locator).toHaveClass() // element has a class property
+        await expect(locator).toHaveCount() // list has exact number of children
+        await expect(locator).toHaveCSS() // element has CSS property
+        await expect(locator).toHaveId() // element has an ID
+        await expect(locator).toHaveJSProperty() // element has a JavaScript property
+        await expect(locator).toHaveRole() // element has a specific ARIA role
+        await expect(locator).toHaveScreenshot() // element has a screenshot
+        await expect(locator).toHaveText() // element matches text
+        await expect(locator).toHaveValue() // input has a value
+        await expect(locator).toHaveValues() // select has options selected
+        await expect(page).toHaveScreenshot() // page has a screenshot
+        await expect(page).toHaveTitle() // page has a title
+        await expect(page).toHaveURL() // page has a URL
+        await expect(response).toBeOK() // response has an OK status
 
-        // Non-retrying assertions
-        expect(value).toBe() - value is the same
-        expect(value).toBeCloseTo() - number is approximately equal
-        expect(value).toBeDefined() - value is not undefined
-        expect(value).toBeFalsy() - value is falsy, e.g. false, 0, null, etc.
-        expect(value).toBeGreaterThan() - number is more than
-        expect(value).toBeGreaterThanOrEqual() - number is more than or equal
-        expect(value).toBeInstanceOf() - object is an instance of a class
-        expect(value).toBeLessThan() - number is less than
-        expect(value).toBeLessThanOrEqual() - number is less than or equal
-        expect(value).toBeNaN() - value is NaN
-        expect(value).toBeNull() - value is null
-        expect(value).toBeTruthy() - value is truthy, i.e. not false, 0, null, etc.
-        expect(value).toBeUndefined() - value is undefined
-        expect(value).toContain() - string contains a substring
-        expect(value).toContain() - array or set contains an element
-        expect(value).toContainEqual() - array or set contains a similar element
-        expect(value).toEqual() - value is similar - deep equality and pattern matching
-        expect(value).toHaveLength() - array or string has length
-        expect(value).toHaveProperty() - object has a property
-        expect(value).toMatch() - string matches a regular expression
-        expect(value).toMatchObject() - object contains specified properties
-        expect(value).toStrictEqual() - value is similar, including property types
-        expect(value).toThrow() - function throws an error
-        expect(value).any() - matches any instance of a class/primitive
-        expect(value).anything() - matches anything
-        expect(value).arrayContaining() - array contains specific elements
-        expect(value).closeTo() - number is approximately equal
-        expect(value).objectContaining() - object contains specific properties
-        expect(value).stringContaining() - string contains a substring
-        expect(value).stringMatching() - string matches a regular expression
+        // Non//retrying assertions
+        expect(value).toBe() // value is the same
+        expect(value).toBeCloseTo() // number is approximately equal
+        expect(value).toBeDefined() // value is not undefined
+        expect(value).toBeFalsy() // value is falsy, e.g. false, 0, null, etc.
+        expect(value).toBeGreaterThan() // number is more than
+        expect(value).toBeGreaterThanOrEqual() // number is more than or equal
+        expect(value).toBeInstanceOf() // object is an instance of a class
+        expect(value).toBeLessThan() // number is less than
+        expect(value).toBeLessThanOrEqual() // number is less than or equal
+        expect(value).toBeNaN() // value is NaN
+        expect(value).toBeNull() // value is null
+        expect(value).toBeTruthy() // value is truthy, i.e. not false, 0, null, etc.
+        expect(value).toBeUndefined() // value is undefined
+        expect(value).toContain() // string contains a substring
+        expect(value).toContain() // array or set contains an element
+        expect(value).toContainEqual() // array or set contains a similar element
+        expect(value).toEqual() // value is similar // deep equality and pattern matching
+        expect(value).toHaveLength() // array or string has length
+        expect(value).toHaveProperty() // object has a property
+        expect(value).toMatch() // string matches a regular expression
+        expect(value).toMatchObject() // object contains specified properties
+        expect(value).toStrictEqual() // value is similar, including property types
+        expect(value).toThrow() // function throws an error
+        expect(value).any() // matches any instance of a class/primitive
+        expect(value).anything() // matches anything
+        expect(value).arrayContaining() // array contains specific elements
+        expect(value).closeTo() // number is approximately equal
+        expect(value).objectContaining() // object contains specific properties
+        expect(value).stringContaining() // string contains a substring
+        expect(value).stringMatching() // string matches a regular expression
       `}</Code>
 
       <Hs>Negating matchers</Hs>
@@ -137,13 +137,13 @@ const postObj = {
 
       <Code block jsx>{`
         // Recommended
-        page.getByRole() to locate by explicit and implicit accessibility attributes.
-        page.getByText() to locate by text content.
-        page.getByLabel() to locate a form control by associated label's text.
-        page.getByPlaceholder() to locate an input by placeholder.
-        page.getByAltText() to locate an element, usually image, by its text alternative.
-        page.getByTitle() to locate an element by its title attribute.
-        page.getByTestId() to locate an element based on its data-testid attribute (other attributes can be configured).
+        page.getByRole() // to locate by explicit and implicit accessibility attributes.
+        page.getByText() // to locate by text content.
+        page.getByLabel() // to locate a form control by associated label's text.
+        page.getByPlaceholder() // to locate an input by placeholder.
+        page.getByAltText() // to locate an element, usually image, by its text alternative.
+        page.getByTitle() // to locate an element by its title attribute.
+        page.getByTestId() // to locate an element based on its data-testid attribute (other attributes can be configured).
 
         // Not recommended
         await page.locator('#tsf > div:nth-child(2) > div.A8SBwf > input').click();

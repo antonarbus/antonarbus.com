4e6227a61f2058cc241ce2b415f6da831dc1e24d
Anton Arbus <anton.arbus@macbookpro>
2025-02-22 15:41:41 +0200

    multiple refs

--- posts/useRef.js
+++ posts/useRef.js
@@ -10,16 +10,21 @@ function Component() {
   const clickRef = React.useRef(0)
   const countClicks = () => clickRef.current++
 
-  const domRef = React.useRef(null)
+  const divRef = React.useRef(null)
   const changeBorderColor = () => {
-    domRef.current.style.borderColor = domRef.current.style.borderColor === 'red' ? 'grey' : 'red'
+    divRef.current.style.borderColor = divRef.current.style.borderColor === 'red' ? 'grey' : 'red'
   }
 
   return (
-    <div style={style} onClick={countClicks} ref={domRef} >
-      <div>Clicks inside the box: <b>{clickRef.current}</b></div>
-      <div>Counter: <b>{clicksState}</b></div>
-      <button onClick={addCounter}>Add +1 to re-render component</button><br />
+    <div style={style} onClick={countClicks} ref={divRef}>
+      <div>
+        Clicks inside the box: <b>{clickRef.current}</b>
+      </div>
+      <div>
+        Counter: <b>{clicksState}</b>
+      </div>
+      <button onClick={addCounter}>Add +1 to re-render component</button>
+      <br />
       <button onClick={changeBorderColor}>Change border color</button>
       {console.log('render happened')}
     </div>
@@ -27,19 +32,83 @@ function Component() {
 }
 // #endregion
 
+// #region multiple refs
+const customRef = {
+  current: null
+}
+// #endregion
+
+export const ComponentWithRef = () => {
+  const externalRef = useRef(null)
+
+  return <MultipleRefs externalRef={externalRef} />
+}
+
+export const MultipleRefs = ({ externalRef }) => {
+  const internalRef = useRef(null)
+
+  return (
+    <>
+      <div
+        ref={(element) => {
+          internalRef.current = element
+          externalRef.current = element
+          customRef.current = element
+        }}
+        css={{
+          background: 'lightgrey'
+        }}
+      >
+        div element
+      </div>
+      <br />
+      <button
+        onClick={() => {
+          const element = internalRef.current
+          const color = element.style.background === 'red' ? 'green' : 'red'
+          element.style.background = color
+        }}
+      >
+        change background <br />
+        with internal ref
+      </button>{' '}
+      <button
+        onClick={() => {
+          const element = externalRef.current
+          const color = element.style.background === 'red' ? 'green' : 'red'
+          element.style.background = color
+        }}
+      >
+        change background <br />
+        with external ref
+      </button>{' '}
+      <button
+        onClick={() => {
+          const element = customRef.current
+          const color = element.style.background === 'red' ? 'green' : 'red'
+          element.style.background = color
+        }}
+      >
+        change background <br />
+        with custom ref
+      </button>
+    </>
+  )
+}
+
 // #region pass ref in props
 function PassRefInProps() {
   const myRef = useRef()
   return (
     <>
-      <Input myRef={myRef}/>
+      <Input myRef={myRef} />
       <button onClick={() => myRef.current.focus()}>Focus</button>
     </>
   )
 }
 
 function Input(props) {
-  return <input ref={props.myRef} placeholder='Custom input'/>
+  return <input ref={props.myRef} placeholder="Custom input" />
 }
 // #endregion
 
@@ -48,13 +117,13 @@ function PassRefViaReactForwardRef() {
   const ref = useRef()
   return (
     <>
-      <MyInputWrappedInForwardRef ref={ref} placeholder='I am a placeholder'/>
+      <MyInputWrappedInForwardRef ref={ref} placeholder="I am a placeholder" />
       <button onClick={() => ref.current.focus()}>Focus</button>
     </>
   )
 }
 function MyInput(props, ref) {
-  return <input ref={ref} {...props}/>
+  return <input ref={ref} {...props} />
 }
 const MyInputWrappedInForwardRef = React.forwardRef(MyInput)
 // #endregion
@@ -66,25 +135,48 @@ const postObj = {
   desc: 'useRef hook',
   body: (
     <>
-      <p><Code js >React.useRef()</Code> <Lnk link={'https://reactjs.org/docs/refs-and-the-dom.html#refs-and-function-components'} text={'hook'} /> plays two roles</p>
-
+      <p>
+        <Code js>React.useRef()</Code>{' '}
+        <Lnk
+          link={'https://reactjs.org/docs/refs-and-the-dom.html#refs-and-function-components'}
+          text={'hook'}
+        />{' '}
+        plays two roles
+      </p>
       <ol>
-        <li> Creates mutable variable, that persists over component renders, but does not trigger a render if it is changed, like <Code>state</Code> </li>
-        <li> Keeps reference to a DOM element if it is passed to <Code>ref</Code> attribute of html element </li>
+        <li>
+          {' '}
+          Creates mutable variable, that persists over component renders, but does not trigger a
+          render if it is changed, like <Code>state</Code>{' '}
+        </li>
+        <li>
+          {' '}
+          Keeps reference to a DOM element if it is passed to <Code>ref</Code> attribute of html
+          element{' '}
+        </li>
       </ol>
-
       <H>Mutable variable</H>
-
-      <p>It is kind of <i>global</i> variable.</p>
-      <p>In our example we count number of clicks and stores it into <Code>clickRef.current</Code> variable</p>
-      <p>The click counter will not trigger a render, but will survive through renders, even if it is defined inside the component.</p>
-      <p>Clicks variable is rendered not with every click, but only when the component renders when we change the state.</p>
-
+      <ul>
+        <li>
+          It is kind of <i>global</i> variable, component based, ones component is unmounted the ref
+          is destroyed.
+        </li>
+        <li>
+          In our example we count number of clicks and store it into <Code>clickRef.current</Code>{' '}
+          variable
+        </li>
+        <li>
+          The click counter will not trigger a render, but will survive through renders, even if it
+          is defined inside the component.
+        </li>
+      </ul>
       <H>Html reference</H>
-
-      <p>We pass <Code>div</Code> element into <Code>domRef</Code> variable via <Code>{'ref={domRef}'}</Code> and can access the element later with{' '} <Code>domRef.current</Code></p>
+      <p>
+        We pass <Code>div</Code> element into <Code>divRef</Code> variable via{' '}
+        <Code>{'ref={divRef}'}</Code> and can access the element later with{' '}
+        <Code>divRef.current</Code>
+      </p>
       <p>In our example we change color of html element with button click.</p>
-
       <Code block>{`
       const style = { border: '2px solid grey', padding: '10px', margin: '10px', maxWidth: '500px' }
 
@@ -95,13 +187,13 @@ const postObj = {
         const clickRef = React.useRef(0)
         const countClicks = () => clickRef.current++
 
-        const domRef = React.useRef(null)
+        const divRef = React.useRef(null)
         const changeBorderColor = () => {
-          domRef.current.style.borderColor = domRef.current.style.borderColor === 'red' ? 'grey' : 'red'
+          divRef.current.style.borderColor = divRef.current.style.borderColor === 'red' ? 'grey' : 'red'
         }
 
         return (
-          <div style={style} onClick={countClicks} ref={domRef} >
+          <div style={style} onClick={countClicks} ref={divRef} >
             <div>Clicks inside the box: <b>{clickRef.current}</b></div>
             <div>Counter: <b>{clicksState}</b></div>
             <button onClick={addCounter}>Add +1 to re-render component</button><br />
@@ -111,17 +203,19 @@ const postObj = {
         )
       }
       `}</Code>
-
       <Component />
-
       <H>Pass ref in props</H>
-
       <ul>
-        <li><Code js >useRef()</Code> can be passed in props, like any other variable</li>
-        <li><Code>ref</Code> attribute can not be used, because this name is reserved by React</li>
-        <li>We use <Code>myRef</Code> attribute</li>
+        <li>
+          <Code js>useRef()</Code> can be passed in props, like any other variable
+        </li>
+        <li>
+          <Code>ref</Code> attribute can not be used, because this name is reserved by React
+        </li>
+        <li>
+          We use <Code>myRef</Code> attribute
+        </li>
       </ul>
-
       <Code block>{`
       function PassRefInProps() {
         const myRef = useRef()
@@ -137,16 +231,17 @@ const postObj = {
         return <input ref={props.myRef} placeholder='Custom input'/>
       }
       `}</Code>
-
       <PassRefInProps />
-
       <H>React.forwardRef()</H>
-
       <ul>
-        <li>We still can use <Code>ref</Code> attribute to pass a ref</li>
-        <li>But need to wrap our component into <Code js >React.forwardRef()</Code> high order component</li>
+        <li>
+          We still can use <Code>ref</Code> attribute to pass a ref
+        </li>
+        <li>
+          But need to wrap our component into <Code js>React.forwardRef()</Code> high order
+          component
+        </li>
       </ul>
-
       <Code block>{`
       function PassRefViaReactForwardRef() {
         const ref = useRef()
@@ -162,9 +257,17 @@ const postObj = {
       }
       const MyInputWrappedInForwardRef = React.forwardRef(MyInput)
       `}</Code>
-
       <PassRefViaReactForwardRef />
+      <H>Multiple refs for html element</H>
+      <ul>
+        <li>Sometimes we may need to add several refs to an element</li>
+        <li>
+          For example we add some functionality to an existing code where ref is already in use
+        </li>
+        <li>We may use a callback variant</li>
+      </ul>
 
+      <ComponentWithRef />
     </>
   )
 }

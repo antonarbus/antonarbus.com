ba89833389b71d54a46d2b37f22cf3f7740f4cf7
Anton Arbus <anton.arbus@macbookpro>
2026-01-23 18:12:03 +0200

    feat: add new post about testing with old Chrome version

---
+++ posts/old_chrome.js
@@ -0,0 +1,55 @@
+'use client'
+
+import {
+  Code,
+  H,
+  Hs,
+  LazyImg,
+  Lnk,
+  React,
+  useEffect,
+  useState,
+  useRef,
+  useCallback,
+  useMemo,
+  jsxToStr,
+  ComponentFromHtmlString
+} from '/components/post/reExport'
+
+const postObj = {
+  title: 'Old chrome version',
+  date: '2026.11.xx',
+  tags: ['tools'],
+  imgUrl: 'https://antonarbus.com/imgs/xxx.png',
+  desc: 'Old chrome verstion',
+  body: (
+    <>
+      <H>Old chrome version</H>
+
+      <ul>
+        <li>Sometimes need to test a polyfill with old browser</li>
+        <li>
+          <Lnk path="https://developer.chrome.com/blog/chrome-for-testing">
+            https://developer.chrome.com/blog/chrome-for-testing
+          </Lnk>
+        </li>
+        <li>
+          <Code inline bash>
+            npx @puppeteer/browsers install chrome@116.0.5793.0
+          </Code>
+        </li>
+      </ul>
+    </>
+  )
+}
+
+export default postObj
+
+export const post = {
+  title: postObj.title,
+  date: postObj.date,
+  tags: postObj.tags,
+  desc: postObj.desc,
+  imgUrl: postObj.imgUrl || null,
+  bodyStr: jsxToStr(postObj.body)
+}

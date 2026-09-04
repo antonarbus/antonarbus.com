ff5ec6797e8192de5c52d9312c832866a166bca3
Anton Arbus <anton.arbus@macbookpro>
2026-09-04 14:25:49 +0300

    feat: enhance SSH post with additional commands and detailed ssh-agent instructions

--- posts/ssh.js
+++ posts/ssh.js
@@ -1,6 +1,5 @@
 'use client'
 
-
 import { Code, H, Hs, Lnk, jsxToStr } from '/components/post/reExport'
 
 const postObj = {
@@ -13,27 +12,74 @@ const postObj = {
       <H>Basic actions on my server</H>
 
       <ul>
-        <li><Code bash>ssh sherb@35.217.12.143</Code> connect</li>
-        <li><Code bash>cd /var/www/html/antonarbus.com</Code> go to this web page folder</li>
-        <li><Code bash>npm i</Code> update packages</li>
-        <li><Code bash>npm run build</Code></li>
+        <li>
+          <Code bash>ssh sherb@35.217.12.143</Code> connect
+        </li>
+        <li>
+          <Code bash>cd /var/www/html/antonarbus.com</Code> go to this web page folder
+        </li>
+        <li>
+          <Code bash>npm i</Code> update packages
+        </li>
+        <li>
+          <Code bash>npm run build</Code>
+        </li>
       </ul>
 
       <H>SSH keys</H>
 
       <ul>
-        <li><Code bash>ls -al ~/.ssh</Code> show files with ssh keys on Unix</li>
-        <li><Code>c:\Users\sherb\.ssh\</Code> same on Windows</li>
-        <li><Code bash>ssh-keygen -t rsa -b 4096 -C "<i>name</i>"</Code> create ssh key</li>
-        <li><code>id_rsa.pub</code> file contains public keys</li>
-        <li><code>id_rsa</code> file contains private keys</li>
+        <li>
+          <Code bash>ls -al ~/.ssh</Code> show files with ssh keys on Unix
+        </li>
+        <li>
+          <Code>c:\Users\sherb\.ssh\</Code> same on Windows
+        </li>
+        <li>
+          <Code bash>
+            ssh-keygen -t rsa -b 4096 -C "<i>name</i>"
+          </Code>{' '}
+          create ssh key
+        </li>
+        <li>
+          <code>id_rsa.pub</code> file contains public keys
+        </li>
+        <li>
+          <code>id_rsa</code> file contains private keys
+        </li>
       </ul>
 
       <Hs>Add keys to GitHub</Hs>
       <ul>
-        <li><Lnk path="https://docs.github.com/en/authentication/connecting-to-github-with-ssh">check this link</Lnk></li>
+        <li>
+          <Lnk path="https://docs.github.com/en/authentication/connecting-to-github-with-ssh">
+            check this link
+          </Lnk>
+        </li>
       </ul>
 
+      <Hs>ssh-agent</Hs>
+      <ul>
+        <li>
+          <code>ssh-agent</code> is a background program that holds unlocked keys in memory, so you
+          don't have to enter the key's passphrase on every connection
+        </li>
+        <li>
+          <Code bash>ssh-add -l</Code> list keys currently loaded into the agent
+        </li>
+        <li>
+          <Code bash>ssh-add ~/.ssh/id_rsa</Code> load a key into the agent (prompts for its
+          passphrase, if any)
+        </li>
+        <li>
+          if <Code bash>ssh-add -l</Code> prints <code>The agent has no identities.</code>, the
+          agent is empty and no key is loaded yet
+        </li>
+        <li>
+          run <Code bash>ssh-add</Code> again after a reboot/new session, or when you get{' '}
+          <code>Permission denied (publickey)</code> errors
+        </li>
+      </ul>
     </>
   )
 }
